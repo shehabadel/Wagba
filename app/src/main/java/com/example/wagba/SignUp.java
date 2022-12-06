6c4f95c9f6174d7254296035d73a83657aaa0082
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2022-12-06 13:31:12 +0200

    Created SignUp Activity

---
+++ app/src/main/java/com/example/wagba/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.wagba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
